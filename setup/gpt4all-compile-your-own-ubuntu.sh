ccb41af2eb56687358c0e481308a2ba38daa3a2f
Hernan Vivani <hernan@vivani.com.ar>
2024-01-21 16:34:23 +0000

    fixing gpt4all compile scripts

--- setup/gpt4all-compile-your-own-ubuntu.sh
+++ setup/gpt4all-compile-your-own-ubuntu.sh
@@ -2,8 +2,8 @@
 ##https://github.com/nomic-ai/gpt4all/issues/682
 
 
-sudo apt-get install build-essential cmake
-git clone --recurse-submodules https://github.com/nomic-ai/gpt4all
+#git clone --recurse-submodules https://github.com/nomic-ai/gpt4all
+git clone --recurse-submodules https://github.com/hvivani/gpt4all.git
 # following the instructions here: https://github.com/nomic-ai/gpt4all/tree/main/gpt4all-bindings/python#local-build-instructions
 cd gpt4all/gpt4all-backend
 mkdir build && cd build
@@ -13,7 +13,6 @@ ls *.so  # should print 'libllmodel.so'
 
 # python bindings:
 cd ../../gpt4all-bindings/python
-sudo apt-get install python3-pip
 # pip install:
 # '-e' means 'editable', so you can edit binding code here, if desired; '.' means the package to install is in this folder
 # i'd strongly recommend '--user' installation, otherwise needs sudo and clobbers your system
--- setup/setup-libraries.sh
+++ setup/setup-libraries.sh
@@ -1,17 +1,17 @@
 #add necessary libraries
-sudo apt-get install libxcb-icccm4 libfontconfig1 libxcb-glx0 libx11-xcb1 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0 libgl1
+sudo apt-get -y install libxcb-icccm4 libfontconfig1 libxcb-glx0 libx11-xcb1 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0 libgl1
 
 #developer libraries
-sudo apt install make g++ pkg-config libgl1-mesa-dev libxcb*-dev libfontconfig1-dev libxkbcommon-x11-dev libgtk-3-dev
+sudo apt -y install make g++ pkg-config libgl1-mesa-dev libxcb*-dev libfontconfig1-dev libxkbcommon-x11-dev libgtk-3-dev
 
 #build esentials
-sudo apt-get install build-essential cmake
+sudo apt-get -y install build-essential cmake
 
 #python 2
-sudo apt-get install python3-pip
+sudo apt-get -y install python3-pip
 
 #adding vulkan libraries
 wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
 sudo apt update
-sudo apt install vulkan-sdk
+sudo apt  -y install vulkan-sdk
